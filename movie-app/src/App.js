27af90ce96dd183244405aa2ed26345da5462075
jung0115 <jungim7490@gmail.com>
2022-11-04 00:42:58 +0900

    7.6까지 완료

---
+++ movie-app/src/App.js
@@ -0,0 +1,20 @@
+// 7.3부터
+import {
+  BrowserRouter as Router,
+  Routes, // Switch (v5) -> Routes (v6)
+  Route
+} from "react-router-dom";
+import Home from "./routes/Home"
+import Detail from "./routes/Detail";
+// 7.7부터 강의 듣기
+
+function App() {
+  return <Router>
+    <Routes>
+      <Route path="/movie/:id" element={<Detail />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>;
+}
+
+export default App;
